a9366bff5056154d2fab074e0914ff97f106fc52
jasperan <23caj23@gmail.com>
2024-01-06 01:29:22 +0100

    feat: added working version for client & server through REST api

--- StreamDiffusion/examples/screen/final_server.py
+++ StreamDiffusion/examples/screen/final_server.py
@@ -10,10 +10,30 @@ from io import BytesIO
 from fastapi import FastAPI
 import uvicorn
 import time
+import argparse
 
 # this code sends base64 images periodically after every screenshot to t_ss.py
 b64_img = []
 
+
+# import monitor settings in settings.py
+from settings import settings_2k, settings_4k, settings_1080p, settings_720p, settings_480p
+
+
+# ask for command line parameters
+cli_parser = argparse.ArgumentParser()
+cli_parser.add_argument('-i', '--ip', type=str, help='IP address to make requests to',
+                        default='127.0.0.1',
+                        required=False)
+
+cli_parser.add_argument('-m', '--mode',
+                        type=str,
+                        help='Execution Mode',
+                        default='2k',
+                        choices=['480p, 720p, 1080p, 2k, 4k'],
+                        required=False)
+
+args = cli_parser.parse_args()
 
 
 app = FastAPI()
@@ -21,9 +41,9 @@ app = FastAPI()
 
 def screen(
     event: threading.Event,
-    height: int = 1024,
-    width: int = 1024,
-    monitor: Dict[str, int] = {"top": 300, "left": 200, "width": 1024, "height": 1024},
+    height: int,
+    width: int,
+    monitor: Dict[str, int],
 ):
     global inputs
     global sct
@@ -69,6 +89,8 @@ def screen(
 
 
 def dummy_screen(
+        top: int,
+        left: int,
         width: int,
         height: int,
 ):
@@ -90,14 +112,29 @@ def dummy_screen(
     return {"top": top, "left": left, "width": width, "height": height}
 
 
+def get_settings(execution_mode):
+    if execution_mode == '480p':
+        return settings_480p
+    elif execution_mode == '720p':
+        return settings_720p
+    elif execution_mode == '1080p':
+        return settings_1080p
+    elif execution_mode == '2k':
+        return settings_2k
+    elif execution_mode == '4k':
+        return settings_4k
+    else: return {}
+
+
 def main():
+
     width: int = 1024
     height: int = 1024
-    monitor = dummy_screen(width, height)
 
-    event = threading.Event()
-    input_screen = threading.Thread(target=screen, args=(event, height, width, monitor))
-    input_screen.start()
+    #get_settings(args.mode)
+    # Now, we don't need the dummy screen anymore:
+    #monitor = dummy_screen(top, left, width, height)
+    monitor = get_settings(args.mode)
 
     #while True:
     '''
@@ -107,24 +144,28 @@ def main():
         response = await websocket.recv()
         print('Received {}'.format(response))
     '''
+
+    
     event = threading.Event()
-    input_screen = threading.Thread(target=screen, args=(event, height, width, monitor))
+    input_screen = threading.Thread(target=screen, args=(event,
+        monitor['height'],
+        monitor['width'],
+        monitor))
     input_screen.start()
+    
         
     input_screen.join()
 
 @app.get("/")
 async def root():
     global b64_img
-    returner_val = b64_img # list type
     
-    returner = list(b64_img)    
+    returner = list(b64_img) # list type
 
-
-    #print(returner)
-    print('Size before clear: {}'.format(len(b64_img)))
+    
+    #print('Size before clear: {}'.format(len(b64_img)))
     b64_img.clear()
-    print('Size after clear: {} vs. |{}|'.format(len(b64_img), len(returner)))
+    #print('Size after clear: {} vs. |{}|'.format(len(b64_img), len(returner)))
     return returner
 
 def run_uvicorn():

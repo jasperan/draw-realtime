1b452f62ca805debeb9aa5c0a8929b3f5e65a1af
jasperan <23caj23@gmail.com>
2024-01-06 23:57:21 +0100

    feat: added customization, throttling, added stablediffusion model, changed prompting, resized images for efficiency

---
+++ StreamDiffusion/examples/screen/settings.py
@@ -0,0 +1,38 @@
+'''
+@author jasperan
+'''
+
+settings_2k = {
+    'top': 0,
+    'left': 0,
+    'width': 2560,
+    'height': 1440
+}
+
+settings_4k = {
+    'top': 0,
+    'left': 0,
+    'width': 3840,
+    'height': 2160
+}
+
+settings_1080p = {
+    'top': 0,
+    'left': 0,
+    'width': 1920,
+    'height': 1080
+}
+
+settings_720p = {
+    'top': 0,
+    'left': 0,
+    'width': 1280,
+    'height': 720
+}
+
+settings_480p = {
+    'top': 0,
+    'left': 0,
+    'width': 640,
+    'height': 480
+}

73e4e8afd10aac392d7c67fbaf2be1c171860b1b
jasperan <23caj23@gmail.com>
2024-01-09 23:23:37 +0100

    feat: updated installation instructions

--- install/conda_windows.sh
+++ install/conda_windows.sh
@@ -1,5 +1,4 @@
 # you will need to manually install node.js
-git clone https://github.com/cumulo-autumn/StreamDiffusion.git
 conda create -n streamdiffusion python=3.10
 conda activate streamdiffusion
 pip3 install torch==2.1.0 torchvision==0.16.0 xformers --index-url https://download.pytorch.org/whl/cu121

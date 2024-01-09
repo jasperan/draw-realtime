fbf914c4e415b1090da933f88b726144122019de
jasperan <23caj23@gmail.com>
2024-01-09 23:23:37 +0100

    feat: updated installation instructions

--- install/conda_linux.sh
+++ install/conda_linux.sh
@@ -1,4 +1,3 @@
-git clone https://github.com/cumulo-autumn/StreamDiffusion.git
 conda create -n streamdiffusion python=3.10
 conda activate streamdiffusion
 pip3 install torch==2.1.0 torchvision==0.16.0 xformers --index-url https://download.pytorch.org/whl/cu121
--- install/docker.sh
+++ install/docker.sh
@@ -1,4 +1,3 @@
-git clone https://github.com/cumulo-autumn/StreamDiffusion.git
-cd StreamDiffusion
+cd ../StreamDiffusion
 docker build -t stream-diffusion:latest -f Dockerfile .
 docker run --gpus all -it -v $(pwd):/home/ubuntu/streamdiffusion stream-diffusion:latest

a73dc43db358a6e9e07c4f18e7b8cc748535485b
jasperan <23caj23@gmail.com>
2024-01-10 10:03:14 +0100

    feat: added multivideo processing support

--- StreamDiffusion/examples/img2img/multi_video.py
+++ StreamDiffusion/examples/img2img/multi_video.py
@@ -3,11 +3,12 @@ import video_processer
 
 def run_main():
     import os
-    dir = '/home/ubuntu/videos/'
-    #dir = '../vid2vid/video/'
+    #dir = '/home/ubuntu/videos/'
+    dir = '../vid2vid/video/'
     for file in os.listdir(dir):
         print(file)
-        video_processer.main(file)
-        thread = threading.Thread(target=video_processer.main, args=(file))
+        video_processer.main('{}{}'.format(dir, file))
+        thread = threading.Thread(target=video_processer.main, args=('{}{}'.format(dir, file)))
         thread.start()
 
+run_main()
